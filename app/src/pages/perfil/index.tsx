f2aa2f0755c7aa5db6f7b67167ed20e6bb92c291
Wesley <wesley.brazil@outlook.com>
2023-05-15 22:58:24 -0300

    fix: api

--- app/src/pages/perfil/index.tsx
+++ app/src/pages/perfil/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+/* eslint-disable */
+import React, { useState, useEffect } from 'react';
 import { 
   Container, 
   Nav, 
@@ -9,15 +10,36 @@ import {
 import { FaArrowLeft } from 'react-icons/fa';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
+import api from '@/services/api';
+
+type DataType = {
+  nome: string | null,
+  dataNascimento: string | null,
+  celular: string | null,
+}
 
 export default function Perfil (){
+  const [data, setData] = useState<DataType>();
 
-  function atualizarPerfil(e: React.FormEvent) {
+  async function atualizarPerfil(e: React.FormEvent) {
     e.preventDefault();
-    console.log('funcionou')
+     const response = await api.post(`/usuario/${data && data.id}`, data);
+
+    if (response && response.data.success) router.back();
   }
 
   const router = useRouter();
+
+  async function pegarUsuario() {
+    const response = await api.get('/usuario').catch(error => console.log('Erro ao consultar usuário', error))
+    console.log("🚀 ~ file: index.tsx:35 ~ pegarUsuario ~ response:", response)
+    if (response) setData(response.data.usuario)
+  }
+  
+  useEffect(() => {
+    pegarUsuario();
+  }, []);
+
   return (
     <Container>
       <Nav>
@@ -29,15 +51,21 @@ export default function Perfil (){
         <Form method='post' onSubmit={(e) => atualizarPerfil(e)}>
           <Input>
             <label htmlFor='nome'>Nome</label>
-            <input id='nome' type="text" />
+            <input id='nome' type="text" value={data && data.nome} onChange={(e) => {
+              setData((item) => ({ ...item, nome: e.target.value }))
+            }} />
           </Input>
           <Input>
             <label htmlFor='data'>Data de nascimento</label>
-            <input id='data' type='date' />
+            <input id='data' type='date' value={data && data.dataNascimento} onChange={(e) => {
+              setData((item) => ({ ...item, dataNascimento: e.target.value }))
+            }} />
           </Input>
           <Input>
             <label htmlFor='celular'>Celular</label>
-            <input id='celular' type="number" />
+            <input id='celular' type="number" value={data && data.celular} onChange={(e) => {
+              setData((item) => ({ ...item, celular: e.target.value }))
+            }} />
           </Input>
           <button type='submit'>Salvar</button>
         </Form>
